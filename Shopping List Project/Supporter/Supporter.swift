092c8cf25ae48f84a82e5472bd8c8cc51172e453
Hamed <hamedullah@gmail.com>
2021-03-08 16:34:20 +0200

    Initial Commit

---
+++ Shopping List Project/Supporter/Supporter.swift
@@ -0,0 +1,8 @@
+//
+//  Supporter.swift
+//  Shopping List Project
+//
+//  Created by Hamed Amiry on 08.03.2021.
+//
+
+import Foundation
---
+++ Shopping List Project/worker/JsonParsing.swift
@@ -0,0 +1,8 @@
+//
+//  JsonParsing.swift
+//  Shopping List Project
+//
+//  Created by Hamed Amiry on 08.03.2021.
+//
+
+import Foundation
